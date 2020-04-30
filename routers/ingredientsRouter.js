bbdd88426f0627146e668f59661aa342250d2f21
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-30 17:52:34 -0600

    Began setting up the routers.

---
+++ routers/ingredientsRouter.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const db = require("../data/config")
+
+const router = express.Router()
+
+/////////////// GET ///////////////
+
+/////////////// POST ///////////////
+
+/////////////// PUT ///////////////
+
+/////////////// DELETE ///////////////
+
+
+
+/////////////// EXPORTS ///////////////
+
+module.exports = router
+
